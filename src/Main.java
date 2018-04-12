6ccba18bff1722f2ae7f61faf02556fa1f407b05
nemb <Niklas@n142-p58.eduroam.kth.se>
2018-04-12 12:26:09 +0200

    working non optimized version

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,26 +11,8 @@ public class Main {
         int E = 0;
         int m = 0;
 
-
-
-        /** Variables for Casting **/
-        int N = 0;
-        int S = 0;
-        int k = 0;
-
-
         /** Edges or Scenes **/
-        int[][] scenes = null;
-
-        /** Check if role is in scene **/
-        boolean[] inScene = null;
-
-        /** Actor not in scene **/
-        ArrayList<Integer> notInScene = new ArrayList<>();
-
-        /** New scenes **/
-        ArrayList<int[]> newScenes = new ArrayList<>();
-
+        ArrayList<int[]> scenes = new ArrayList<>();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
@@ -43,19 +24,17 @@ public class Main {
                 // Num Vertices
                 case 0:
                 {
-                    V = Integer.parseInt(line);
-                    inScene = new boolean[V];
+                    V = Integer.parseInt(line) + 2;
                     break;
                 }
                 // Num Edges
                 case 1:{
                     E = Integer.parseInt(line);
-                    scenes = new int[E][2];
                     break;
                 }
                 // Num colors
                 case 2:{
-                    m = Integer.parseInt(line);
+                    m = Integer.parseInt(line) + 2;
                     break;
                 }
                 // Connected Edges
@@ -65,76 +44,33 @@ public class Main {
                     String[] vertices = line.split(" ");
                     int v1 = Integer.parseInt(vertices[0]);
                     int v2 = Integer.parseInt(vertices[1]);
-                    scenes[index][0] = v1;
-                    scenes[index][1] = v2;
-                    inScene[v1-1] = true;
-                    inScene[v2-1] = true;
-
+                    int[] scene = {v1, v2};
+                    scenes.add(scene);
                 }
             }
             lineCounter++;
         }
+        
+        int[] scene = new int[2];
+        scene[0] = V;
+        scene[1] = V-1;
+        scenes.add(scene);
 
-
-        /** Check isolated **/
-        int isolated = 0;
-        for (int i = 0; i < inScene.length; i++) {
-            if (!inScene[i]) {
-                isolated++;
-                notInScene.add(i+1);
-
-            }
+        for(int i = 1; i < V-1; i++){
+            scene = new int[2];
+            scene[0] = V-1;
+            scene[1] = i;
+            scenes.add(scene);
         }
 
+        E = scenes.size();
 
-        if(isolated > 1){
-            /** Add edges to nodes that are isolated **/
-            if(isolated % 2 == 0){
-                for (int i = 0; i < isolated; i++) {
-                    int[] scene = new int[2];
-                    scene[0] = notInScene.get(i);
-                    i++;
-                    scene[1] = notInScene.get(i);
-                    newScenes.add(scene);
-
-                }
-            }
-            else{
-                for (int i = 1; i < isolated; i++) {
-                    int[] scene = new int[2];
-                    scene[0] = notInScene.get(0);
-                    scene[1] = notInScene.get(i);
-                    newScenes.add(scene);
-
-                }
-                int[] scene = new int[2];
-                scene[0] = notInScene.get(0);
-                scene[1] = scenes[0][0];
-                newScenes.add(scene);
-
-            }
-        }else if(isolated == 1) {
-            int[] scene = new int[2];
-            scene[0] = notInScene.get(0);
-            scene[1] = scenes[0][0];
-            newScenes.add(scene);
-
-        }
-        m = m + isolated;
-
-
-        /** Check if V is less than number of colors **/
-        if(m > V){
-            m = V;
-        }
-
-        printInstance(V, E, m, scenes, isolated, newScenes);
-
-
+        printInstance(V, E, m, scenes);
     }
-    public static void printInstance(int V, int E, int m, int[][] scenes, int isolated, ArrayList<int[]> newScenes){
+
+    public static void printInstance(int V, int E, int m, ArrayList<int[]> scenes){
         System.out.println(V);
-        System.out.println(E + newScenes.size());
+        System.out.println(E);
         System.out.println(m);
         StringBuilder sb = new StringBuilder();
         sb.append(m);
@@ -146,23 +82,9 @@ public class Main {
             System.out.println(sb.toString());
         }
 
-        if(E == 0){
-            for (int i = 0; i < newScenes.size()-1; i++) {
-                System.out.println(2 + " " + newScenes.get(i)[0] + " " + newScenes.get(i)[1]);
-            }
-            System.out.print(2 + " " + newScenes.get(newScenes.size()-1)[0] + " " + newScenes.get(newScenes.size()-1)[1]);
-
-        }else{
-            for (int i = 0; i < E-1; i++) {
-                System.out.println(2 + " " + scenes[i][0] + " " + scenes[i][1]);
-            }
-            if (isolated > 0){
-                for (int i = 0; i < newScenes.size(); i++) {
-                    System.out.println(2 + " " + newScenes.get(i)[0] + " " + newScenes.get(i)[1]);
-                }
-            }
-            System.out.print(2 + " " + scenes[E-1][0] + " " + scenes[E-1][1]);
+        for (int i = 0; i < E-1; i++) {
+            System.out.println(2 + " " + scenes.get(i)[0] + " " + scenes.get(i)[1]);
         }
-
+        System.out.print(2 + " " + scenes.get(E-1)[0] + " " + scenes.get(E-1)[1]);
     }
 }
